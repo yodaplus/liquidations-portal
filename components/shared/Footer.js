f84bc0e0a59bc223045bd28b7c05287a4d084e2e
Taras Yevtushenko <taras.evt@gmail.com>
2022-03-28 14:20:53 +0300

    clean up the footer

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -207,139 +207,28 @@ class LongFooter extends React.Component {
         >
           <div>
             <LinkListHeader>{t('Resources')}</LinkListHeader>
-            <LinkList>
-              <li>
-                <Link to="https://makerdao.com/whitepaper">{t('Whitepaper')}</Link>
-              </li>
-              <li>
-                <Link to="https://awesome.makerdao.com/#faqs">{t('FAQs')}</Link>
-              </li>
-              <li>
-                <Link to="https://makerdao.com/privacy">{t('Privacy Policy')}</Link>
-              </li>
-              <li>
-                <Link to="https://www.notion.so/makerdao/Maker-Brand-ac517c82ff9a43089d0db5bb2ee045a4">
-                  {t('Brand Assets')}
-                </Link>
-              </li>
-              <li>
-                <Link to="https://makerdao.com/en/feeds/">{t('Feeds')}</Link>
-              </li>
-              <li>
-                <Link to="https://makerdao.statuspage.io/">{t('Service Status')}</Link>
-              </li>
-            </LinkList>
+            <LinkList></LinkList>
           </div>
           <div>
             <LinkListHeader>{t('Products')}</LinkListHeader>
             <LinkList>
               <li>
-                <Link to="https://oasis.app/">{t('Oasis')}</Link>
-              </li>
-              <li>
-                <Link to={'https://migrate.makerdao.com/'}>{t('Migrate')}</Link>
-              </li>
-              <li>
-                <Link to={'https://makerdao.com/en/ecosystem/'}>{t('Ecosystem')}</Link>
-              </li>
-              <li>
-                <Link to="https://makerdao.com/en/governance/">{t('Governance')}</Link>
+                <Link to="https://borrow.yodaplus.net/">{t('Borrow')}</Link>
               </li>
             </LinkList>
           </div>
           <div>
             <LinkListHeader>{t('Developers')}</LinkListHeader>
-            <LinkList>
-              <li>
-                <Link to="https://docs.makerdao.com/" external>
-                  {t('Documentation')}
-                </Link>
-              </li>
-              <li>
-                <Link to="https://docs.makerdao.com/dai.js">Dai.js</Link>
-              </li>
-              <li>
-                <Link to="https://github.com/makerdao/developerguides">Developer Guides</Link>
-              </li>
-            </LinkList>
+            <LinkList></LinkList>
           </div>
           <div>
             <LinkListHeader>{t('Foundation')}</LinkListHeader>
-            <LinkList>
-              <li>
-                <Link to="https://makerdao.com/contact">{t('Contact')}</Link>
-              </li>
-            </LinkList>
+            <LinkList></LinkList>
           </div>
           <Box gridColumn={['1 / 3', '1 / 3', 'unset']}>
-            <Flex sx={{ width: '284px', flexDirection: 'row' }} mt="1.2rem">
-              <Input
-                maxWidth="320px"
-                py="xs"
-                placeholder="Sign up for our newsletter"
-                sx={{
-                  fontSize: 15,
-                  borderRadius: '5px 0px 0px 5px',
-                  bg: 'surface',
-                  '::placeholder': {
-                    color: 'formGrey',
-                    opacity: 1,
-                    fontWeight: '200'
-                  },
-                  ':focus': { borderColor: 'secondary' }
-                }}
-                name="email"
-                type="email"
-                value={this.state.email}
-                onChange={evt => this.updateEmail(evt)}
-                errorMessage={(this.state.subscribeState === 'failure' || undefined) && this.state.message}
-                successMessage={(this.state.subscribeState === 'success' || undefined) && this.state.message}
-              />
-              <Button
-                variant="outline"
-                type="submit"
-                onClick={this.subscribeEmail}
-                sx={{
-                  padding: '0px',
-                  paddingTop: '4px',
-                  width: '44px',
-                  border: '1px solid',
-                  borderColor: 'secondary',
-                  borderLeft: 0,
-                  borderRadius: '0px 5px 5px 0px',
-                  ':hover': { borderColor: 'secondary' },
-                  ':hover svg': { transform: 'translate(0.25rem)' },
-                  svg: {
-                    margin: '0 auto',
-                    transition: 'transform 0.125s'
-                  }
-                }}
-              >
-                <Icon name="subscribe_arrow" />
-              </Button>
-            </Flex>
             <Icons gap="20px" mt="1.8rem" columns="repeat(7, 20px)">
-              <Link to="https://twitter.com/MakerDAO">
-                <Icon name="twitter" />
-              </Link>
-              <Link to="https://www.reddit.com/r/MakerDAO/">
-                <Icon name="reddit" />
-              </Link>
-              <Link to="https://t.me/makerdaoOfficial">
-                <Icon name="telegram" />
-              </Link>
-              <Link to="https://chat.makerdao.com/">
-                <Icon name="rocket_chat" />
-              </Link>
-              {/*<div>
-                <Icon
-                  name='we_chat'
-                  sx={{ cursor: 'pointer' }}
-                  onClick={this.toggleWeChatModal}
-                />
-              </div>*/}
-              <Link to="https://www.youtube.com/MakerDAO">
-                <Icon name="youtube" />
+              <Link to="https://github.com/yodaplus">
+                <Icon name="github" />
               </Link>
             </Icons>
             <Box mt="1.8rem" maxWidth="180px">
@@ -347,25 +236,6 @@ class LongFooter extends React.Component {
             </Box>
           </Box>
         </Grid>
-        {/* <Modal
-          show={this.state.showWeChatModal}
-          onClose={this.toggleWeChatModal}
-          width='380px'
-        >
-          <Flex flexDirection='column' alignItems='center'>
-            <QRCode
-              bgColor='#FFFFFF'
-              fgColor='#000000'
-              level='Q'
-              style={{ width: '245px', height: '245px' }}
-              value='http://weixin.qq.com/r/tjh0bADEN11IrUQP922k'
-            />
-            <Box fontSize='1.8rem'>{t('wechat-scan-text')}</Box>
-            <Button mt='m' onClick={this.toggleWeChatModal}>
-              Close
-            </Button>
-          </Flex>
-        </Modal> */}
       </FooterContainer>
     );
   }
@@ -385,29 +255,8 @@ export default function Foot({ shorten = false }) {
           fontSize: 2
         }}
       >
-        <NavLink href="/terms" target="_blank" variant="footer" p={1} sx={{ fontWeight: 400 }}>
-          Terms
-        </NavLink>
-        <NavLink
-          href="https://makerdao.com/en/privacy/"
-          target="_blank"
-          variant="footer"
-          p={2}
-          sx={{ fontWeight: 400 }}
-        >
-          Privacy Policy
-        </NavLink>
-        <NavLink
-          href="https://makerdao.statuspage.io/"
-          target="_blank"
-          variant="footer"
-          p={2}
-          sx={{ fontWeight: 400 }}
-        >
-          Status
-        </NavLink>
         <div sx={{ mx: 'auto' }} />
-        <div sx={{ p: 2 }}>© 2021 Maker</div>
+        <div sx={{ p: 2 }}>© 2022 Yodaplus</div>
       </footer>
     );
   } else {
