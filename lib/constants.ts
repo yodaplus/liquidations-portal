ed6a218be783aff680ded8145e391a39c398ec86
Sudeep <49141455+rsud33p@users.noreply.github.com>
2022-09-28 17:10:11 +0530

    Set Apothem as the default network

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,7 +6,7 @@ export enum SupportedNetworks {
   XINFIN_MAINNET = 'xinfin-mainnet'
 }
 
-export const DEFAULT_NETWORK = SupportedNetworks.XINFIN_MAINNET;
+export const DEFAULT_NETWORK = SupportedNetworks.APOTHEM;
 
 export const ETHERSCAN_PREFIXES = {
   [SupportedNetworks.XINFIN_MAINNET]: 'xdc.',
